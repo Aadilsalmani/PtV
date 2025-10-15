4ead1df2c886f3b4bb5da178fac86eb91dc658dc
Aadilsalmani <72613731+Aadilsalmani@users.noreply.github.com>
2025-10-15 23:12:01 +0530

    tourist data replaced

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@ const FILES_TO_CACHE = [
   "./index.html",
   "./css/style.min.css",
   "./js/script.min.js",
-  "./data/tourist_data.json",
+  "https://script.google.com/macros/s/AKfycbw6ZXWi9U1pzumYL-o7aUSPoJSGekdCrL7mEkLaFaVQU803q5LqsW5_UXVZH8PR11jV/exec",
   "./manifest.json",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
