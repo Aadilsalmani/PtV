733c513735c4386e4a07c683b4c305c6704f4bb9
Aadilsalmani <aadilsalmani65@gmail.com>
2026-05-22 20:19:09 +0530

    Nomad Fixing Attempt 2

--- functions/index.js
+++ functions/index.js
@@ -12,9 +12,14 @@ admin.initializeApp();
 const db = admin.firestore();
 
 const app = express();
-app.use(cors({ origin: true }));
 
-app.use(cors());
+app.use(cors({
+  origin: true,
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.options('*', cors());
 
 app.use(express.json({ limit: '1mb' }));
 
